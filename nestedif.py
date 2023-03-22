88a4144bfdf25589133fc0dcb27d0f6e9973cc40
Srechass <128572059+Srechass@users.noreply.github.com>
2023-03-22 11:29:36 +0530

    Create nestedif.py

---
+++ nestedif.py
@@ -0,0 +1,13 @@
+per=55
+if per>=60:
+    if per>=80:
+        print("Distinction")
+    else:
+        print("First class")
+elif per>=50:
+    print("Second clss")
+elif per>=40:
+    print("Third class")
+else:
+    print("fail")
+print("Exam Results")
